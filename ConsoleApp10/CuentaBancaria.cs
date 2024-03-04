6e13ecd36378ae439282ab0b4114016799b50bc3
inicio <inicio@FISEP17>
2024-03-04 14:37:48 -0600

    Agregar archivos de proyecto.

---
+++ ConsoleApp10/CuentaBancaria.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad_2_Banco
+{
+    internal class CuentaBancaria
+    {
+        private int NumCuenta;
+        private int Saldo;
+        private string Nombre;
+
+        public CuentaBancaria(int numCuenta, int saldo, string nombre)
+        {
+            NumCuenta = numCuenta;
+            Saldo = saldo;
+            Nombre = nombre;
+        }
+
+        public void Operacion()
+        {
+            Console.WriteLine("Que operacion deseas realizar");
+            Console.WriteLine("1(Retirar)");
+            Console.WriteLine("2(Depositar)");
+            Console.WriteLine("3(Consultar saldo)");
+            int opcion = 0;
+            Console.WriteLine(" ");
+            opcion = int.Parse(Console.ReadLine());
+
+            int saldo = Saldo;
+            int reti = 0, resta = 0, depo = 0, suma = 0;
+            switch (opcion)
+            {
+                case 1:
+                    Console.WriteLine("Retirar");
+                    reti = int.Parse(Console.ReadLine());
+                    resta = saldo - reti;
+                    Console.WriteLine("Su saldo ahora es: " + resta);
+                    break;
+                case 2:
+                    Console.WriteLine("Depositar");
+                    depo = int.Parse(Console.ReadLine());
+                    suma = saldo + depo;
+                    Console.WriteLine("Su saldo aumento a: " + suma);
+                    break;
+                case 3:
+                    Console.WriteLine("Consultar saldo");
+                    Console.WriteLine("Su saldo es de " + saldo);
+
+                    break;
+                default:
+                    Console.WriteLine("Esa operacion no existe!");
+                    break;
+            }
+
+        }
+    }
+}
---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Actividad_2_Banco;
+
+Console.WriteLine("Buenos dias somos el banco BEN10");
+CuentaBancaria cuenta = new CuentaBancaria(2254, 500, "Leopoldo");
+
+cuenta.Operacion();
